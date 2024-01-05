6fc23c8cef44a3fe88b95e9bcdc1aac85bcc8705
SUNATH KHADIKAR <43642660+sunath2711@users.noreply.github.com>
2024-01-05 16:10:07 +0530

    Create Longest_increasing_subsequence.py
    Very important question - revisit twice / thrice - uses binary srch and DP appraoch

---
+++ Longest_increasing_subsequence.py
@@ -0,0 +1,27 @@
+#Very IMP QUES
+# Given an integer array nums, return the length of the longest strictly increasing 
+# subsequence
+
+# Input: nums = [10,9,2,5,3,7,101,18]
+# Output: 4
+# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) / 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+        
