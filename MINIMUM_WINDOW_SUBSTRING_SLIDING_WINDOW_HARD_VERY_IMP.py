e31202f8783bbae52b61c3fa114cf045fc1ebb43
SUNATH KHADIKAR <43642660+sunath2711@users.noreply.github.com>
2024-02-04 19:25:42 +0530

    Create MINIMUM_WINDOW_SUBSTRING_SLIDING_WINDOW_HARD_VERY_IMP.py
    VERY IMPORTANT - HARD ques

---
+++ MINIMUM_WINDOW_SUBSTRING_SLIDING_WINDOW_HARD_VERY_IMP.py
@@ -0,0 +1,27 @@
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring
+#  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+# The testcases will be generated such that the answer is unique.
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need, missing = collections.Counter(t), len(t)
+        i = I = J = 0
+        for j, c in enumerate(s, 1):
+            missing -= need[c] > 0
+            need[c] -= 1
+            if not missing:
+                while i < j and need[s[i]] < 0:
+                    need[s[i]] += 1
+                    i += 1
+                if not J or j - i <= J - I:
+                    I, J = i, j
+        return s[I:J]
+
+#very important question . mother of all sliding window problems
