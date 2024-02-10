27da19808f112d5fa18474aaf65ec6f91ddd53b8
SUNATH KHADIKAR <43642660+sunath2711@users.noreply.github.com>
2024-02-10 23:03:30 +0530

    Create Palindromic_substring_counts_DP_MEDIUM.py
    MEDIUM - but IMPORTANT

---
+++ Palindromic_substring_counts_DP_MEDIUM.py
@@ -0,0 +1,38 @@
+# Given a string s, return the number of palindromic substrings in it.
+
+# A string is a palindrome when it reads the same backward as forward.
+
+# A substring is a contiguous sequence of characters within the string.
+
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        count = 0
+        # Create a 2D DP table to store whether substrings are palindromic or not
+        dp = [[False] * n for _ in range(n)]
+        
+        # All single characters are palindromes
+        for i in range(n):
+            dp[i][i] = True
+            count += 1
+        
+        # Check for substrings of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                count += 1
+        
+        # Check for substrings of length > 2
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    count += 1
+        
+        return count
+        
