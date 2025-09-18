9e64c90b8fbfb4e2a718117592f911d0ddc62bb7
SUNATH KHADIKAR <43642660+sunath2711@users.noreply.github.com>
2025-09-18 22:38:40 +0530

    Update containsDuplicate1.py

--- containsDuplicate1.py
+++ containsDuplicate1.py
@@ -1,6 +1,18 @@
+Set method
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         N = len(nums)
         S = len(set(nums))
 
         return not N == S
+
+Hashmap method
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        map = {}
+        for i in nums:
+            if i in map:
+                return True
+            else:
+                map[i] = 1
+        return False            
